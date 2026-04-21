d21a53a1c5bef914d89a3d240142dd1d2b66689b
Martin Bailey <martin.bailey@education.gov.uk>
2026-04-21 13:37:21 +0100

    docs

--- docs-site/lib/helpers.rb
+++ docs-site/lib/helpers.rb
@@ -1,23 +1,3 @@
 require 'nanoc/helpers/link_to'
-require 'redcarpet'
 
 use_helper Nanoc::Helpers::LinkTo
-
-module RepoReadmeHelper
-  def repo_readme_html
-    markdown = File.read(File.expand_path('../../README.md', __dir__))
-    renderer = Redcarpet::Render::HTML.new(hard_wrap: true)
-    parser = Redcarpet::Markdown.new(
-      renderer,
-      autolink: true,
-      fenced_code_blocks: true,
-      no_intra_emphasis: true,
-      strikethrough: true,
-      tables: true
-    )
-
-    parser.render(markdown)
-  end
-end
-
-use_helper RepoReadmeHelper
